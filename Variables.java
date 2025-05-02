96dd3e869494dc8af2bceb1bd955e17faf354dd1
ELMRABET-Abdelali <78224898+ELMRABET-Abdelali@users.noreply.github.com>
2025-05-02 21:25:28 +0100

    Variables.java

---
+++ Variables.java
@@ -0,0 +1,42 @@
+// 1. Declarer et utiliser une variable age 
+class Main {
+    public static void main(String[] args) {
+        int age ;
+        age = 25;
+
+        System.out.println("I am " + age + " years old.");
+    }
+}
+
+// 2. idem mais dans la meme ligne 
+
+class Main {
+    public static void main(String[] args) {
+        int age = 25; // Initialize age with a value
+
+        System.out.println("I am " + age + " years old.");
+    }
+}
+
+// 3. variable déclarée mais non initialisé
+
+class Main {
+    public static void main(String[] args) {
+        int age = 25; // Initialize age with a value
+
+        System.out.println("I am " + age + " years old.");
+    }
+}
+
+// 4. changement de valeur de variable
+
+class Main {
+    public static void main(String[] args) {
+        int age = 25; // Initialize age with a value
+        age = 40;
+        System.out.println("I am " + age + " years old.");
+    }
+}
+
+
+
