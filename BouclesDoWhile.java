7a044ffc390ce3dd49983c8b752e71b76b7c9692
ELMRABET-Abdelali <78224898+ELMRABET-Abdelali@users.noreply.github.com>
2025-05-03 04:34:26 +0100

    Create BouclesDoWhile.java

---
+++ BouclesDoWhile.java
@@ -0,0 +1,43 @@
+// 1. example de do while , faire tant que 
+
+class Main {
+    public static void main(String[] args) {
+        int number = 5;
+        int multiplier = 1;
+
+        do {
+            System.out.printf("%d x %d = %d%n", number, multiplier, number * multiplier);
+            multiplier++;
+        } while (multiplier <= 10);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
