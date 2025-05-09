37c9d24711bdd0c006c258bd58b4cfd07ed1c284
ELMRABET-Abdelali <78224898+ELMRABET-Abdelali@users.noreply.github.com>
2025-05-09 18:52:49 +0100

    Update Main.java

--- POO/Main.java
+++ POO/Main.java
@@ -7,8 +7,8 @@ class Main {
         // user.name = "amine saad";
         // user.birthDay = LocalDate.parse("1995-01-31");
 
-        Book book = new Book("carmilla", "Sheridan Le Fanu");
-                      
+        Book book = new Book("carmilla", "Sheridan Le Fanu", 270);
+
         // book.title = "Carmilla";
         // book.author = "Sheridan Le Fanu";
 
@@ -24,21 +24,16 @@ class Main {
                           user.name, 
                           user.books.toString());
 
-        System.out.printf("%s a emprunté ces livres : %s \n", user.name, user.books.toString());
-                      
-    }
-}
-// 2
-class Main {
-    public static void main(String[] args) {
+
         Book carmilla = new Book("Carmilla", "Sheridan Le Fanu", 270);
         AudioBook dracula = new AudioBook("Dracula", "Bram Stoker", 400, 30000);
         Ebook jeeves = new Ebook("Carry On Jeeves", "P.G. Wodehouse", 280, "PDF");
-
         System.out.println(jeeves.toString());
         System.out.println(carmilla.toString());
         System.out.println(dracula.toString());
+
     }
 }
+
 
 
