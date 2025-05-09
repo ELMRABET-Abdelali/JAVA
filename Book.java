95b3711dec8b145c9108fded1ed55ea259dbd105
ELMRABET-Abdelali <78224898+ELMRABET-Abdelali@users.noreply.github.com>
2025-05-09 18:07:18 +0100

    Update Book.java

--- Book.java
+++ Book.java
@@ -1,8 +1,17 @@
 public class Book {
     public String title;
-    public String author;
+    // public String author;
+    private String author;
 
+    Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+    
     public String toString() {
         return String.format("%s by %s", this.title, this.author);
     }
+
+
 }
+
