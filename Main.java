9a23a904e4170a7a2f4a36b4ab673cfa7cb49415
ELMRABET-Abdelali <78224898+ELMRABET-Abdelali@users.noreply.github.com>
2025-05-09 15:49:02 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,13 +3,23 @@ import java.time.LocalDate;
 class Main {
     public static void main(String[] args) {
         User youngerUser = new User();
-
-        youngerUser.name = "Farhan Hasin Junior";
-        youngerUser.birthDay = LocalDate.parse("1995-01-31");
-
-        System.out.printf("%s was born back in %s and he is now %d years old.", 
+        
+        youngerUser.name = "amine said";
+        youngerUser.birthDay = LocalDate.parse("1998-01-01");
+        
+        System.out.printf("%s est né le %s et il a maintenant %d ans.%n", 
                           youngerUser.name, 
                           youngerUser.birthDay.toString(), 
                           youngerUser.age());
+
+        User olderUser = new User();
+        
+        olderUser.name = "siham kinan";
+        olderUser.birthDay = LocalDate.parse("1979-01-01");
+        
+        System.out.printf("%s est née le %s et elle a maintenant %d ans.%n", 
+                          olderUser.name, 
+                          olderUser.birthDay.toString(), 
+                          olderUser.age());
     }
 }
