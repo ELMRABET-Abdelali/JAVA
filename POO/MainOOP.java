40d75eedb2604147a4a6196986067af9205c4498
ELMRABET-Abdelali <78224898+ELMRABET-Abdelali@users.noreply.github.com>
2025-05-17 01:53:21 +0100

    Update MainOOP.java

--- POO/MainOOP.java
+++ POO/MainOOP.java
@@ -641,8 +641,163 @@ public class User {
   
 }
 
+// 11. Extending a Class with Inheritance
 
+import java.util.ArrayList;
+import java.util.List;
 
+// Classe principale
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        // Liste des étudiants
+        Student.admins = new ArrayList<Student>();
+        
+        // Ajout des étudiants dans la liste des admins
+        Student.admins.add(new Student("Jack", "bronze"));
+        Student.admins.add(new Student("Jill", "gold"));
+        Student.admins.add(new Student("Jake", "silver"));
+                      
+        // Affichage du nom des étudiants
+        System.out.println(Student.admins.get(0).get_name());
+        System.out.println(Student.admins.get(1).get_name());
+
+        System.out.println("printing students :");
+        for (Student student : Student.admins){
+            System.out.println(student.get_name());
+        }
+    }
+}
+
+// Classe Student
+public class Student extends User {
+    // Liste des étudiants administrateurs
+    public static List<Student> admins = new ArrayList<>();
+
+    // Constructeurs
+    public Student() {
+        set_name("default");
+    }
+
+    public Student(String name, String memberShip) {
+        set_name(name);
+        setMemberShip(memberShip);
+    }
+}
+
+// Classe User
+public class User {
+    private String name;
+    String memberShip;
+
+    void set_name(String name) {
+        this.name = name;
+    }
+
+    String get_name() {
+        return this.name;
+    }
+
+    void setMemberShip(String memberShip) {
+        this.memberShip = memberShip;
+    }
+
+    public String getMemberShip() {
+        return memberShip;
+    }
+
+    public String toString() {
+        return String.format("%s is a %s member", this.name, this.memberShip);
+    }
+
+    public boolean equals(User u2) {
+        return this.name.equals(u2.name) && this.memberShip.equals(u2.memberShip);
+    }
+}
+
+// 12. polymorphisme
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        // Create instances of Student and Teacher
+        Student s = new Student();
+        s.set_name("student");
+
+        Teacher t = new Teacher();
+        t.set_name("teacher");
+
+        // Initialize the list of admins
+        User.admins = new ArrayList<User>();
+        User.admins.add(s);
+        User.admins.add(t);
+        User.admins.add(new User("Caleb"));
+        User.admins.add(new User("You"));
+        User.admins.add(new User("Sally"));
+
+        // Iterate over the list to verify users
+        for (User u : User.admins) {
+            u.verify();
+        }
+    }
+}
+
+// User class
+class User {
+    protected String name;
+    private boolean _verified = false;
+    public static List<User> admins;
+
+    public User() {
+        set_name("default");
+    }
+
+    public User(String name) {
+        set_name(name);
+    }
+
+    void set_name(String name) {
+        this.name = name;
+    }
+
+    String get_name() {
+        return this.name;
+    }
+
+    void set_verified(boolean verified) {
+        _verified = verified;
+    }
+
+    boolean get_verified() {
+        return _verified;
+    }
+
+    void verify() {
+        System.out.println("User verification process");
+        set_verified(true);
+    }
+}
+
+// Student class
+class Student extends User {
+    @Override
+    void verify() {
+        System.out.println("Verifying through email for student: " + get_name());
+        set_verified(true);
+    }
+}
+
+// Teacher class
+class Teacher extends User {
+    @Override
+    void verify() {
+        System.out.println("Teacher verified: " + get_name());
+        set_verified(true);
+    }
+}
 
 
 
